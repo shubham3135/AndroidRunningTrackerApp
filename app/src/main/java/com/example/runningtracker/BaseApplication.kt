c0cc87e8564ef503872a5c673cdc6d502b950ba2
Shubham <shubhamkumarwinner@gmail.com>
2021-05-31 13:44:00 +0530

    Setup Room Dagger-hilt

---
+++ app/src/main/java/com/example/runningtracker/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.example.runningtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+}
--- app/src/main/java/com/example/runningtracker/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.runningtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.runningtracker.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject lateinit var runDao: RunDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //for testing hilt
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
