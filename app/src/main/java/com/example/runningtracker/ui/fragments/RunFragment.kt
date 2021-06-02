74b49c2b4f9858864c8a58eb740e0a5f44c023d1
Shubham <shubhamkumarwinner@gmail.com>
2021-06-02 13:29:17 +0530

    Sorting Runs

--- app/src/main/java/com/example/runningtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/RunFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.runningtracker.R
 import com.example.runningtracker.adapters.RunAdapter
 import com.example.runningtracker.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.runningtracker.other.SortType
 import com.example.runningtracker.other.TrackingUtility
 import com.example.runningtracker.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,7 +32,38 @@ class RunFragment: Fragment(R.layout.fragment_run), EasyPermissions.PermissionCa
         requestPermissions()
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+        when(viewModel.sortType){
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.DISTANCE -> spFilter.setSelection(2)
+            SortType.AVG_SPEED -> spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spFilter.setSelection(4)
+        }
+
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                adapeterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position){
+                    0->viewModel.sortRuns(SortType.DATE)
+                    1->viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2->viewModel.sortRuns(SortType.DISTANCE)
+                    3->viewModel.sortRuns(SortType.AVG_SPEED)
+                    4->viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //not necessary
+                //not necessary
+            }
+
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
         fab.setOnClickListener {
@@ -66,7 +99,10 @@ class RunFragment: Fragment(R.layout.fragment_run), EasyPermissions.PermissionCa
         }
     }
 
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        //not necessary
+        //not necessary
+    }
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
         if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)){
